c11bffaef5ea2024e5ea82269a21856fefbacb0e
helphen <920484857@qq.com>
2021-05-08 00:48:45 +0800

    add

--- opensource/depend.sh
+++ opensource/depend.sh
@@ -1,6 +1,8 @@
 sudo dpkg-reconfigure dash
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
+sudo apt-get install aptitude -y
+sudo aptitude install lib32stdc++6-4.8-dbg -y
 sudo apt-get install make -y
 sudo apt-get install build-essential -y
 sudo apt-get install libncurses5 -y
