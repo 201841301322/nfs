11574df9fbe1233475084679ae6b937e630a4c2c
helphen <920484857@qq.com>
2021-05-08 00:47:06 +0800

    add

---
+++ opensource/depend.sh
@@ -0,0 +1,8 @@
+sudo dpkg-reconfigure dash
+sudo apt-get install gcc -y
+sudo apt-get install g++ -y
+sudo apt-get install make -y
+sudo apt-get install build-essential -y
+sudo apt-get install libncurses5 -y
+sudo apt-get install libncurses5-dev -y
+sudo apt-get install u-boot-tools -y
