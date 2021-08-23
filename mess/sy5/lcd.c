fce5c52762a9ef9d49cd68744377052422f684f4
halphen <920484857@qq.com>
2021-08-23 10:36:33 +0000

    update lcd

--- mess/sy5/lcd.c
+++ mess/sy5/lcd.c
@@ -13,6 +13,98 @@ static char color_buf[800*480*4]={0};
 
 int fd_lcd=-1,i=0,j;
 
+int get_sta()
+{
+	int fd_ts;
+
+	unsigned int x,y;
+
+	struct input_event buf;
+	fd_ts=open("/dev/input/event2",O_RDWR);
+
+	if(fd_ts < 0)
+	{
+		printf("open ts error\r\n");
+		
+		return -1;
+		
+	}
+	while(1)
+	{
+		
+		
+		//读取触摸屏设备的数据
+		read(fd_ts, &buf, sizeof buf);
+		
+		//如果是绝对坐标值的事件类型
+		if(buf.type == EV_ABS|| buf.type==EV_KEY)
+		{
+			if(buf.code == ABS_X)
+			{
+				x = buf.value* 800/1024;
+				printf("x_buf=%d\n",x);
+				
+			}
+			if(buf.code == ABS_Y)
+			{
+				y = buf.value* 480/600;
+			}
+			if(buf.type == EV_KEY && buf.code == BTN_TOUCH )
+			{
+			//松开
+			if(buf.value == 0)
+			{
+				end_x=x;
+				printf("end_x=%d\n",end_x);
+
+				printf("release!\n");
+				
+				}       
+
+				
+				
+			}
+			//按下
+			else
+			{	
+				
+				star_x=x;
+				star_y=y;
+				
+
+				printf("x=%d\n",x);
+				printf("y=%d\n",y);
+				printf("down!\n");
+				
+				
+			}
+		}
+
+
+			if(buf.code == ABS_X)
+			{
+				x = buf.value* 800/1024;
+				printf("x=%d\n",x);
+			}
+			
+			if(buf.code == ABS_Y)
+			{
+				
+				y = buf.value* 480/600;
+				//printf("y=%d\n",y);
+				
+				
+			} 
+			
+
+		}
+		
+		
+		
+	}
+	return 0;
+}
+
 int play(char b,char g ,char r)
 {
 	fd_lcd=open("/dev/fb0",O_RDWR);
